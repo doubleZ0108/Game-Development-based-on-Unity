66f773485558be48e1ffd942dbf6204e51fba4be
double Z <doubleZ0108@163.com>
2020-03-23 19:03:46 +0800

    :alembic:muzzle destroy

---
+++ demo/TankGame/Muzzle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muzzle : MonoBehaviour
+{
+    public float lifeTime = 5;
+
+    void Start()
+    {
+        //子弹自销毁
+        StartCoroutine(SelfDestroyCoroutine(lifeTime));
+    }
+
+    private IEnumerator SelfDestroyCoroutine(float time)
+    {
+        float elapsed = 0.0f;
+        while(elapsed < time)
+        {
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        Debug.Log("destroy...");
+        Destroy(gameObject);
+    }
+
+    
+}
